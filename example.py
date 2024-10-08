2129372153409917bae47355c7bb9dec3e9b26d5
Matheus Gonçalves <32453656+mathgonsa@users.noreply.github.com>
2024-10-08 13:11:29 -0300

    Delete example.py

--- example.py
+++
@@ -1,8 +0,0 @@
-from src.core.learn import learn
-from src.core.reply import generate_reply
-
-chat_id = "-1001726117958"
-message = "Maskara partir pra tomar uma skoooooooooool"
-
-learn(chat_id, message)
-print("> Reply:", generate_reply(chat_id, message))
